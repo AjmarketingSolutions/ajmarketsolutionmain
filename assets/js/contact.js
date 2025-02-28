06996b61606ea5809213b2044d57168b8f8e95aa
AjmarketingSolutions <ajmarketingsolutionstvm@gmail.com>
2025-02-28 17:14:05 +0530

    work finished

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,26 +1,29 @@
-  // Initialize EmailJS
-  (function() {
-    emailjs.init('Mh8q33tv27kmx51eq'); // Replace YOUR_USER_ID with your EmailJS user ID
-  })();
+(function(){
+    emailjs.init("AFz5CZ07_wyVZZxxh"); 
+})();
 
-  // Add event listener to the form
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
+function sendMail(event) {
+    event.preventDefault();
 
-    // Show the loading text
-    document.querySelector('.loading').style.display = 'block';
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
 
-    // Send the form data to EmailJS
-    emailjs.sendForm('service_ckta25e', 'template_0ekkf4d', this)
-      .then(function(response) {
-        console.log('SUCCESS', response);
-        // Hide the loading text and show the success message
-        document.querySelector('.loading').style.display = 'none';
-        document.querySelector('.sent-message').style.display = 'block';
-      }, function(error) {
-        console.log('FAILED', error);
-        // Hide the loading text and show the error message
-        document.querySelector('.loading').style.display = 'none';
-        document.querySelector('.error-message').style.display = 'block';
-      });
-  });
+    emailjs.send("service_ckta25e", "template_0ekkf4d", parms)
+        .then(function() {
+            alert("Email Sent Successfully");
+        }, function(error) {
+            alert("Failed to send email. Error: " + JSON.stringify(error));
+        });
+}
+
+
+
+
+
+
+
+
